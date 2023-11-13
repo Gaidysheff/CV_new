7bc263643c62c4c6a0844343f5b8d627063dc4a0
Gaidysheff <gaidysheff@mail.ru>
2023-11-13 20:42:05 +0500

    Static connection is improved

--- Resume/Resume/settings.py
+++ Resume/Resume/settings.py
@@ -106,6 +106,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -113,6 +115,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = []
+
--- Resume/Resume/urls.py
+++ Resume/Resume/urls.py
@@ -1,7 +1,28 @@
+# from django.conf.urls import url
+# from django.conf.urls.static import static
+# from Resume import settings
+# from cv.views import *
 from django.contrib import admin
 from django.urls import path, include
+
+# from django.views.static import serve as mediaserve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cv.urls')),
 ]
+
+# if settings.DEBUG:
+#     import debug_toolbar
+#
+#     urlpatterns = [
+#         path('__debug__/', include(debug_toolbar.urls)),
+#     ] + urlpatterns
+#
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# else:
+#     urlpatterns += [
+#         url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$', mediaserve, {'document_root': settings.MEDIA_ROOT}),
+#         url(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$', mediaserve, {'document_root': settings.STATIC_ROOT}),
+#     ]
